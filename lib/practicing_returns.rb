492b42dc07a3c69f52890e84c76a34dda96ca951
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-24 01:53:22 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def hello(array)
   i = 0
-  while i < array.length    #while used to iterate over an array
+  while i < array.length
     binding.pry
-    yield(array[i])     #each time we loop/iterate, yield each element
+    yield(array[i])   
     i += 1
   end
 end
